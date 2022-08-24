1fbfc008722255c8616e03a6a43e0821f402820d
Cecilia Valim <cissamadalenarv@gmail.com>
2022-08-23 21:06:46 -0300

    Added "place order" scenario and steps

--- src/test/java/pageObjects/PageObjectManager.java
+++ src/test/java/pageObjects/PageObjectManager.java
@@ -7,6 +7,7 @@ public class PageObjectManager {
     public WebDriver driver;
     public LandingPage landingPage;
     private OffersPage offersPage;
+    private CheckoutPage checkoutPage;
 
     public PageObjectManager(WebDriver driver){
         this.driver = driver;
@@ -20,5 +21,10 @@ public class PageObjectManager {
     public OffersPage offersPage(){
         offersPage = new OffersPage(driver);
         return offersPage;
+    }
+
+    public CheckoutPage getCheckoutPage(){
+        checkoutPage = new CheckoutPage(driver);
+        return checkoutPage;
     }
 }
