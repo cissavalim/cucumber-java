6baf78858bac811c54e6276e7652a0e3554899b2
Cecilia Valim <cissamadalenarv@gmail.com>
2022-08-31 22:05:50 -0300

    Added props configurations

--- src/test/java/utils/TestBase.java
+++ src/test/java/utils/TestBase.java
@@ -12,16 +12,19 @@ public class TestBase {
 
     public WebDriver driver;
 
-
     public WebDriver WebDriverManager() throws IOException {
 
         FileInputStream fis = new FileInputStream(System.getProperty("user.dir")+"/src/test/resources/global.properties");
         Properties prop = new Properties();
         prop.load(fis);
         String url = prop.getProperty("QAURL");
+        String browser_props = prop.getProperty("browser");
+        String browser_maven = System.getProperty("browser");
+
+        String browser = browser_maven != null ? browser_maven : browser_props;
 
         if(driver == null){
-            if(prop.getProperty("browser").equalsIgnoreCase("chrome")) {
+            if(browser.equalsIgnoreCase("chrome")) {
                 System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/src/test/resources/chromedriver.exe");
                 driver = new ChromeDriver();
             }
